bce9935636c491a77eae486af6d21592fc403a37
StraetenMax <maxstraeten@yahoo.com>
2025-11-22 15:01:50 +0100

    modifiction et correction des fichiers gulpfile et package.json

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,35 +3,20 @@ import pug from 'gulp-pug';
 import mjml from 'mjml';
 import { minify as htmlmin } from 'html-minifier-terser';
 import rename from 'gulp-rename';
-import clean from 'gulp-clean';
+import { deleteAsync } from 'del';
 import through2 from 'through2';
-// import htmlhint from 'gulp-htmlhint';  // Commenté, à décommenter si nécessaire
 import { load } from 'cheerio';
-import liveServer from 'live-server';
+import browserSync from 'browser-sync';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { promises as fs } from 'fs';
-// import { mkdir } from 'fs/promises';  // Commenté car fs.mkdir est déjà importé ci-dessus
 
-// Définit __dirname
+// Définir
+const bs = browserSync.create();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Configuration simple pour MJML
-const config = {
-  mjmlOptions: {
-    beautify: false, // Ne pas embellir le HTML généré
-    minify: false,   // Ne pas minifier le HTML
-    validationLevel: 'strict', //'soft', 'skip' Niveau de validation strict
-    fonts: {}, // Configuration des polices
-    keepComments: false, // Ne pas conserver les commentaires
-    ignoreIncludes: true, // Ignorer les inclusions
-    preprocessors: [], // Liste des préprocesseurs
-    useMjmlConfigOptions: false, // Ne pas utiliser les options de configuration MJML
-  }
-};
-
-// Tâche pour supprimer les attributs style
+// Tâche pour supprimer les attributs style vides
 const removeEmptyStyles = () => {
   return through2.obj((file, _, cb) => {
     if (file.isBuffer()) {
@@ -47,38 +32,33 @@ const removeEmptyStyles = () => {
 const ensureDistDirectory = async () => {
   try {
     await fs.mkdir('./dist', { recursive: true });
-    console.log('Directory "dist" created or already exists.');
+    console.log('Répertoire "dist" créé ou existe déjà.');
   } catch (error) {
-    console.error('Error creating directory "dist":', error);
+    console.error('Erreur lors de la création du répertoire "dist":', error);
   }
 };
 
-// Serveur
+// Démarrage du serveur browerSync avec désactivation du cache
 const serve = (done) => {
-  const params = {
+  bs.init({
+    server: {
+      baseDir: './dist'
+    },
     port: 8080,
-    root: path.resolve(__dirname, './dist'),
-    open: true,
-    file: 'index.html',
-    wait: 500,
-    logLevel: 2, // Niveau de journalisation (0 = désactivé, 1 = erreurs, 2 = infos, 3 = debogage)
-  };
-  try {
-    liveServer.start(params);
-    console.log('Live Server started on port', params.port);
-  } catch (error) {
-    console.error('Error starting Live Server:', error);
-  }
+    open: false,
+    files: ['./dist/*.html'],
+    notify: false,
+    ghostMode: false
+  });
   done();
 };
 
-// Nettoyage
+// Nettoyage du répertoire 'dist'
 const cleanDist = () => {
-  return gulp.src('./dist', { allowEmpty: true, read: false })
-    .pipe(clean());
+  return deleteAsync(['./dist/*', '!./dist/images']);
 };
 
-// Pug vers Mjml
+// Conversion de Pug à MJML
 const pugToMjml = () => {
   return gulp.src('./src/*.pug')
     .pipe(pug({
@@ -92,25 +72,21 @@ const pugToMjml = () => {
     .pipe(gulp.dest('./src/mjml'));
 };
 
-// Mjml vers HTML
+// Conversion de MJML à HTML
 const mjmlToHtml = () => {
   return gulp.src('./src/mjml/*.mjml')
     .pipe(through2.obj((file, _, cb) => {
       try {
         const mjmlContent = file.contents.toString();
-        // Utilise la configuration définie plus haut
-        const mjmlConfig = {
-          ...config.mjmlOptions,
+        const result = mjml(mjmlContent, {
           filePath: file.path // Ajout du chemin du fichier pour les imports relatifs
-        };
-        
-        const result = mjml(mjmlContent, mjmlConfig);
-        
+        });
+
         if (result.errors && result.errors.length) {
-          console.error('MJML Errors:', result.errors);
-          return cb(new Error('MJML compilation failed'));
+          console.error('Erreurs MJML:', result.errors);
+          return cb(new Error('Compilation MJML échouée'));
         }
-        
+
         file.contents = Buffer.from(result.html);
         cb(null, file);
       } catch (error) {
@@ -127,16 +103,15 @@ const mjmlToHtml = () => {
 // Minification HTML
 const minifyHtml = () => {
   return new Promise((resolve) => {
-    // Petit délai pour s'assurer que les fichiers sont bien créés
-    setTimeout(() => {
-      console.log('Starting minifyHtml task...');
+    setTimeout(async () => {
+      console.log('Démarrage de la tâche minifyHtml...');
       gulp.src(['./dist/*.html', '!./dist/*.min.html'])
         .pipe(through2.obj(async (file, enc, callback) => {
           if (file.isBuffer()) {
             try {
               const minified = await htmlmin(String(file.contents), {
                 collapseWhitespace: true,
-                removeComments: false, // On garde false pour les commentaires conditionnels
+                removeComments: false, // Garder false pour les commentaires conditionnels
                 removeEmptyAttributes: true,
                 minifyCSS: true,
                 conservativeCollapse: false, // Changé à false pour minifier plus agressivement
@@ -148,19 +123,18 @@ const minifyHtml = () => {
                 html5: false // Important pour la compatibilité email
               });
               file.contents = Buffer.from(minified);
-              //console.log(`Minified file: ${file.path}`);
             } catch (error) {
-              console.error(`Error minifying file: ${file.path}`, error);
+              console.error(`Erreur lors de la minification du fichier: ${file.path}`, error);
             }
           } else {
-            console.warn(`File is not a buffer: ${file.path}`);
+            console.warn(`Le fichier n'est pas un buffer: ${file.path}`);
           }
           callback(null, file);
         }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist'))
         .on('end', () => {
-          console.log('minifyHtml task completed.');
+          console.log('Tâche minifyHtml terminée.');
           resolve();
         });
     }, 500); // Délai de 500ms
@@ -175,7 +149,7 @@ const customFilesize = () => {
       const fileName = path.basename(file.path);
       console.log(`${fileName}: ${fileSizeInKB.toFixed(2)} Ko`);
     } else {
-      console.warn(`File is not a buffer: ${file.path}`);
+      console.warn(`Le fichier n'est pas un buffer: ${file.path}`);
     }
     cb(null, file);
   });
@@ -184,21 +158,24 @@ const customFilesize = () => {
 const verification = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      console.log('Starting verification task...');
+      console.log('Démarrage de la tâche de vérification...');
       gulp.src('dist/*.html')
         .pipe(customFilesize())
         .pipe(gulp.dest('dist'))
         .on('end', () => {
-          console.log('verification task completed.');
+          console.log('Tâche de vérification terminée.');
           resolve();
         });
     }, 500); // Délai de 500ms
   });
 };
 
-// Watch
+// Surveillance des modifications
 const watch = () => {
-  gulp.watch('./src/**/*.pug', gulp.series(pugToMjml, mjmlToHtml, minifyHtml, verification));
+  gulp.watch('./src/**/*.pug', gulp.series(pugToMjml, mjmlToHtml, minifyHtml, verification, (done) => {bs.reload;
+      done();
+    }
+  ));
 };
 
 // Tâche par défaut
@@ -208,10 +185,8 @@ const defaultTask = gulp.series(
   pugToMjml,
   mjmlToHtml,
   (done) => {
-    setTimeout(() => {
       gulp.series(minifyHtml, verification, serve, watch)(done);
-    }, 500);
-  }
+    }
 );
 
 // Export des tâches
